2aed8d50a92527d8fe6687ca76c7e4246046be4d
LILYKLOPCIC <LILYKLOPCIC@GMAIL.COM>
2020-12-03 18:52:17 -0500

    added hover

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -1,17 +1,20 @@
 import "./Aboutme.css";
 import React, { Component } from "react";
 import Headshot from "../images/headshot.png";
-import HtCs from '../images/html_css.png';
+import Javascript from "../images/javascript.png";
+import Python from "../images/python.png";
+import Express from "../images/express.svg";
+import Django from "../images/django.png";
+import Reac from "../images/react.png";
 class Aboutme extends Component {
   render() {
     return (
       <div className="aboutmesection">
-       
         <h5 className="bio">
-        <h5 className='aboutme'>About Me</h5>
-        <div>
-           <img className="headshot" src={Headshot} alt="headshot" />
-           </div>
+          <h5 className="aboutme">About Me</h5>
+          <div>
+            <img className="headshot" src={Headshot} alt="headshot" />
+          </div>
           Lily Klopcic is an enterprising full-stack engineer, experienced
           dancer and box office manager, and connoisseur of musical theatre, red
           wine and high fashion. Brought up in the respective worlds of
@@ -20,10 +23,15 @@ class Aboutme extends Component {
           detail and fine aesthetic, her goal is to take the experiences of her
           upbringing to the tech industry and contribute to a new wave of
           invention in art, technology, and culture.
+          <div><h4 className='aboutme'>My Skills</h4>
+          <img className="javascript" src={Javascript} />
+          <img className="python" src={Python} />
+          <img className="express" src={Express} />
+          <img className="django" src={Django} />
+          <img className="reac" src={Reac} />
+          </div>
         </h5>
-       
       </div>
-
     );
   }
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,6 +9,11 @@ class Projects extends Component {
       var win = window.open(bodyworksUrl, "_blank");
       win.focus();
     }
+    function openBodyworksRepoNewTab() {
+      const bodyworksRepoUrl = "https://github.com/xbry12/group-project-repo";
+      var win = window.open(bodyworksRepoUrl, "_blank");
+      win.focus();
+    }
     return (
       
       <div className="projectsDiv">
@@ -18,7 +23,10 @@ class Projects extends Component {
          <a onClick={openBodyworksNewTab}>
           <img className="bodyworks" src={Bodyworks} alt="BodyWorks Project" />
         </a> 
-       
+       <h5 className='projectdescription'>BodyWorks is an application using MERN Stack with full CRUD functuality. 
+       <a onClick={openBodyworksRepoNewTab}>
+       <button className='repolink'>Link to Repo</button>
+        </a>  </h5>
       </div>
      
     );
